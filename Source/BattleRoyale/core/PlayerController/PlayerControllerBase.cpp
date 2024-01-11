296fe32cf2c3512200814422ab55ca6233f0f0d4
DESKTOP-D3G5C6G\dguerrero <dguerrero_bcn@hotmail.com>
2024-01-11 19:03:56 +0100

    Calculating server time from client.

--- Source/BattleRoyale/core/PlayerController/PlayerControllerBase.cpp
+++ Source/BattleRoyale/core/PlayerController/PlayerControllerBase.cpp
@@ -1,7 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "PlayerControllerBase.h"
+
 
 void APlayerControllerBase::ClientEnableInput_Implementation(bool enable)
 {
@@ -14,7 +13,64 @@ void APlayerControllerBase::EnableInput(bool enable)
 	ClientEnableInput(enable);
 }
 
+float APlayerControllerBase::GetServerTime() const
+{
+	const auto currentTime = GetWorld()->GetTimeSeconds();
+	
+	if(HasAuthority())
+	{
+		return currentTime;
+	}
+	else
+	{
+		return currentTime + mClientServerDelta;
+	}
+}
+
+void APlayerControllerBase::ReceivedPlayer()
+{
+	Super::ReceivedPlayer();
+
+	if(IsLocalController())
+	{
+		ServerRequestServerTime(GetWorld()->GetTimeSeconds());
+	}
+}
+
 void APlayerControllerBase::BeginPlay()
 {
 	Super::BeginPlay();
 }
+
+void APlayerControllerBase::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	CheckTimeSync(DeltaSeconds);
+}
+
+void APlayerControllerBase::CheckTimeSync(float DeltaSeconds)
+{
+	mTimeSyncRunningTime += DeltaSeconds;
+	if(IsLocalController() && mTimeSyncRunningTime > mTimeSyncFrequency)
+	{
+		ServerRequestServerTime(GetWorld()->GetTimeSeconds());
+		mTimeSyncRunningTime = 0.0f;
+	}
+}
+
+void APlayerControllerBase::ServerRequestServerTime_Implementation(float timeOfClientRequest)
+{
+	const auto serverTimeOfReceipt = GetWorld()->GetTimeSeconds();
+	ClientReportServerTime(timeOfClientRequest, serverTimeOfReceipt);
+}
+
+void APlayerControllerBase::ClientReportServerTime_Implementation(float timeOfClientRequest,
+	float timeServerReceivedClientRequest)
+{
+	const auto currentTime = GetWorld()->GetTimeSeconds();
+	const auto roundTripTime = currentTime - timeOfClientRequest;
+	const auto currentServerTime = timeServerReceivedClientRequest + 0.5f * roundTripTime;
+	mClientServerDelta = currentServerTime - currentTime;
+}
+
--- Source/BattleRoyale/core/PlayerController/PlayerControllerBase.h
+++ Source/BattleRoyale/core/PlayerController/PlayerControllerBase.h
@@ -18,12 +18,34 @@ class BATTLEROYALE_API APlayerControllerBase : public APlayerController, public 
 
 public:
 	virtual void EnableInput(bool enable) override;
+	float GetServerTime() const;
+	virtual void ReceivedPlayer() override; //Sync with server clock as soon as possible.
 
 protected:
 	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
 
 private:
 	UFUNCTION(Client, Reliable)
 	void ClientEnableInput(bool enable);
 
+	/**
+	 * Sync time between Client and Server
+	 */
+	
+	//Request the current server time, passing in the client's time when the request was sent
+	UFUNCTION(Server, Reliable)
+	void ServerRequestServerTime(float timeOfClientRequest);
+
+	//Reports the current server time to the client in response to ServerRequestServerTime
+	UFUNCTION(Client, Reliable)
+	void ClientReportServerTime(float timeOfClientRequest, float timeServerReceivedClientRequest);
+	void CheckTimeSync(float DeltaSeconds);
+	
+	float mClientServerDelta = 0.0f; //Difference between Client and Server time.
+
+	UPROPERTY(EditAnywhere, Category = Time)
+	float mTimeSyncFrequency = 5.0f;
+
+	float mTimeSyncRunningTime = 0.0f;
 };
